6bd4d205511988fffb6b3ed8b807784f68eeab09
Randy Miller <randy918@gmail.com>
2022-07-15 00:28:10 -0500

    mediocre boxes

---
+++ .history/js/main_20220714192509.js
@@ -0,0 +1,16 @@
+$(function () {
+    $("#list").first("li").filter(":even").css("background-color", "red");
+
+
+});
+const main = function () {
+  c(74);
+};
+
+const init = function () {
+  main();
+};
+
+window.onload = function () {
+  init();
+};
