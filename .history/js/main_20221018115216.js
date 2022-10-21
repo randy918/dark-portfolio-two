8e92e1246de1742a0d76a30c34e3f1a90f614973
Randy Miller <randy918@gmail.com>
2022-10-21 12:53:06 -0500

    attempted 102122 reset

---
+++ .history/js/main_20221018115216.js
@@ -0,0 +1,30 @@
+const main = function () {
+
+    [].forEach.call($$("*"),function(a){a.style.outline="1px solid #"+(~~(Math.random()*(1<<24))).toString(16)})
+  new Swiper("#swiper-1", {
+    effect: "cube", // delete for sideways motion, use fade
+    autoplay: {
+      delay: 6500,
+    },
+    pagination: {
+        el: "#swiper-1 .swiper-pagination",
+        clickable: true,
+    },
+    lazyloading: true,
+    loop: true,
+  });
+
+
+
+
+  
+};
+
+const init = function () {
+  c(74);
+  main();
+};
+
+window.onload = function () {
+  init();
+};
---
+++ .history/js/main_20221018115218.js
@@ -0,0 +1,30 @@
+const main = function () {
+
+    
+  new Swiper("#swiper-1", {
+    effect: "cube", // delete for sideways motion, use fade
+    autoplay: {
+      delay: 6500,
+    },
+    pagination: {
+        el: "#swiper-1 .swiper-pagination",
+        clickable: true,
+    },
+    lazyloading: true,
+    loop: true,
+  });
+
+
+
+
+  
+};
+
+const init = function () {
+  c(74);
+  main();
+};
+
+window.onload = function () {
+  init();
+};
